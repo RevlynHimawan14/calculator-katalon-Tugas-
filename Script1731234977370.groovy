278a4b97250929fcc4330c26852b37850af4e515
RevlynHimawan14 <156493301+RevlynHimawan14@users.noreply.github.com>
2024-11-18 09:51:41 +0700

    Add files via upload

---
+++ Script1731234977370.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('Mobile App/Google Calculator_8.6.1.apk', false)
+
+Mobile.delay(3, FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Hitung Perkalian/number_7_Button'), 0)
+
+Mobile.tap(findTestObject('Hitung Perkalian/cross_button'), 0)
+
+Mobile.tap(findTestObject('Hitung Perkalian/number_1_Button'), 0)
+
+Mobile.tap(findTestObject('Hitung Perkalian/number_5_Button'), 0)
+
+Mobile.tap(findTestObject('Hitung Perkalian/result_Button'), 0)
+
